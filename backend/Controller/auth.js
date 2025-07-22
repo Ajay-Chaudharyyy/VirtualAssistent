529cac036c4afdb503b7362d8a7595c141653c3a
Ajay Chaudharyyy <ajayawana4308@gmail.com>
2025-07-22 10:25:14 +0530

    login issue fixed

--- backend/Controller/auth.js
+++ backend/Controller/auth.js
@@ -40,7 +40,7 @@ exports.signUp = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      maxAge: 3 * 24 * 60 * 60 * 1000,
+      maxAge: 3 *24 * 60 * 60 * 1000,
       sameSite: "None",
       secure: true,
     });
@@ -84,9 +84,9 @@ exports.login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      maxAge: 3 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: false,
+      maxAge: 3 *24 * 60 * 60 * 1000,
+      sameSite: "None",
+      secure: true,
     });
     return res.status(200).json({
       success: true,
